b3cedff6d5be31642b36661f61acfe785946e43e
Kerstin Vater <kerstin.vater@tuhh.de>
2022-06-30 20:53:53 +0200

    closes #1; know my enemy now

--- I_beam_2d_static_mesh_convergence_study.m
+++ I_beam_2d_static_mesh_convergence_study.m
@@ -4,29 +4,36 @@
 clear
 close all
 
+% Load custom colormaps
+load("colors\viridis.mat")
+load("colors\Set1.mat")
+set(0, "DefaultAxesColorOrder", Set1)
+
+fprintf('\nSetting up cantilever beam Finite Element model...');
+
 % Create static analysis model for 2-D plane-stress problem
-modelStatic = createpde("structural", "static-planestress")
+modelStatic = createpde("structural", "static-planestress");
 
 %% Create Geometry
 
-L = 2.0;    % Beam length
-h = 0.01;   % Beam height
+L = 2.0;    % Beam length       [m]
+h = 0.01;   % Beam height       [m]
 I = h^3/12; % Moment of inertia
 
 x = [0.; L; L; 0.];
 y = [-h/2; -h/2; h/2; h/2];
 
-geometryFromEdges(modelStatic, decsg([3, 4, x' y']'))
+geometryFromEdges(modelStatic, decsg([3, 4, x' y']'));
 
 % Plot geometry
 figure
 sgtitle("Cantilever beam 2D static Finite Element analysis")
 set(gcf, "WindowState", "maximized")
-ax = subplot(2,2,1)
+ax = subplot(2,2,1);
 pdegplot(modelStatic, "VertexLabels", "on", "EdgeLabels", "on");
 title('Geometry with labels')
-xlabel("x")
-ylabel("y")
+xlabel("x [m]")
+ylabel("y [m]")
 xlim([0. 2.2])
 ylim([-0.06 0.06])
 grid on
@@ -35,26 +42,28 @@ ax.DataAspectRatio = [1 0.1 0.1];
 %% Preprocess
 
 % Define material properties
-E   = 2e11;     % Young's modulus
-nu  = 0.3;      % Poisson's ratio
-rho = 1000.;    % Mass density
+E   = 2e11;     % Young's modulus   [Pa]
+nu  = 0.3;      % Poisson's ratio   [-]
+rho = 1000.;    % Mass density      [kg/m^3]
 
-structuralProperties(modelStatic, "YoungsModulus", E, "PoissonsRatio", nu, "MassDensity", rho)
+structuralProperties(modelStatic, "YoungsModulus", E, "PoissonsRatio", nu, "MassDensity", rho);
 
 % Specify left edge of the beam as fixed boundary
 structuralBC(modelStatic, "Edge", 4, "Constraint", "fixed");
 
 % Apply static vertical load corresponding to maximum static friction force
 % at the right side of the beam
-Fcrit = 50;
+Fcrit = 50; % Point force value [N]
 structuralBoundaryLoad(modelStatic, 'Vertex', 2, 'Force', [0; Fcrit]);
+
+fprintf(' done.\n\n');
 
 % Compute analytical solution
 deltaRef = (Fcrit * L^3) / (3 * E * I);
 % Note: Euler-Bernoulli beam theory
 % https://en.wikipedia.org/wiki/Euler%E2%80%93Bernoulli_beam_theory (accessed 2022-06-30)
 % https://en.wikipedia.org/wiki/List_of_second_moments_of_area (accessed 2022-06-30)
-fprintf('Expected max. deflection: delta = %6.4f m.\n', deltaRef);
+fprintf('The analytical deflection at the beam''s free end is %6.4f m.\n\n', deltaRef);
 
 %% Solve
 
@@ -65,7 +74,10 @@ errorLimit = 7e-3;
 hmax = h .* 200; % Initial value for target maximum element edge size
 i = 1;
 
-ax = subplot(2,2,2)
+ax = subplot(2,2,2);
+
+fprintf('Starting mesh convergence study...\n\n');
+
 while true
 
     % Create mesh
@@ -73,9 +85,9 @@ while true
 
     % Plot mesh
     pdeplot(modelStatic)
-    xlabel("x")
-    ylabel("y")
-    title("Finite Element mesh")
+    xlabel("x [m]")
+    ylabel("y [m]")
+    title("Finite Element mesh (h_{max} = " + num2str(hmax) + " m)")
     xlim([0. 2.2])
     ylim([-0.06 0.06])
     grid on
@@ -90,9 +102,12 @@ while true
 
     numNodes(i) = size(modelStatic.Mesh.Nodes, 2);
     err(i) = abs(deltaRef-delta) / abs(deltaRef) * 100;
-    fprintf('h = %6.4f, nodes = %8d, e = %6.4f %%.\n', hmax, numNodes(i), err(i));
+    fprintf('hmax = %8.4f m, nodes = %8d, deflection = %12.4e m, error = %8.4f %%.\n', hmax, numNodes(i), delta, err(i));
 
-    if err(i) < errorLimit; break; end
+    if err(i) < errorLimit
+        fprintf('\nThe predefined error limit of %6.4f %% has been reached.\nThe mesh convergence study was completed successfully.\n', errorLimit);
+        break;
+    end
 
     i = i + 1;
     hmax = hmax .* 0.7; % Reduce element size by 30%
@@ -102,7 +117,7 @@ end
 %% Postprocess
 
 % Plot mesh convergence behavior
-subplot(2,2,3)
+ax = subplot(2,2,3);
 loglog(numNodes, err, "Marker", "o", "MarkerSize", 5.0)
 hold on
 loglog(numNodes, (1./numNodes.^1).*2e2, "LineStyle", "--")
@@ -116,13 +131,14 @@ legend('Rel. error', '1/n^1', '1/n^2')
 grid off
 
 % Plot deformed cantilever beam
-ax = subplot(2,2,4)
+ax = subplot(2,2,4);
 pdeplot(modelStatic, "XYData", staticRes.Displacement.uy, ...
-    "Deformation", staticRes.Displacement, 'DeformationScaleFactor', 1e0, ...
-    "ColorMap", "Jet");
-xlabel("x")
-ylabel("y")
-title('y-displacements')
+    "Deformation", staticRes.Displacement, ...
+    'DeformationScaleFactor', 1e0, ...
+    "ColorMap", viridis);
+xlabel("x [m]")
+ylabel("y [m]")
+title('y-displacements [m]')
 xlim([0. 2.2])
 ylim([-0.06 0.06])
 grid on
